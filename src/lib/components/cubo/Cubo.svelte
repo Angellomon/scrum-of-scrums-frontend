862f0871b6d7f882059e64ba15c27e8dbb9961ef
Angellomon <angelmtzdiaz@gmail.com>
2021-08-23 17:21:37 -0500

    feat(cubo): implementación de cubo

---
+++ src/lib/components/cubo/Cubo.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Face from './Face.svelte';
+</script>
+
+<div class="cubo">
+  <span class="top">
+    <Face skewX={30} skewY={0} rotation={-30} scaleY={0.87} />
+  </span>
+  <span class="left">
+    <Face top={30} left={-100} skewX={30} skewY={0} rotation={30} />
+  </span>
+  <span class="right">
+    <Face top={-273} left={101} skewX={-30} skewY={0} rotation={-30} />
+  </span>
+</div>
+
+<style>
+  div.cubo {
+    position: relative;
+  }
+</style>
