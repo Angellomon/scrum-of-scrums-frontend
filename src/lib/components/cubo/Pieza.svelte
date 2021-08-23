1acb76cacc16a24b9ed9e59e8fd087ef61ebd439
Angellomon <angelmtzdiaz@gmail.com>
2021-08-23 17:20:04 -0500

    feat(cubo): implementación de Unidad

--- src/lib/components/cubo/Pieza.svelte
+++ src/lib/components/cubo/Pieza.svelte
@@ -1,12 +1,46 @@
-<div>
-</div>
+<script>
+  import { onDestroy } from 'svelte';
+
+  export let colorPrincpal = 'bfea45';
+  export let on = false;
+
+  const colores = ['bfea45', 'def4a5', 'dee5e9', 'b6dcf9', '3b726c', 'fdf6b8', 'aaacef', 'b4c6f2'];
+
+  const obtenerColor = () => {
+    return colores[Math.floor(Math.random() * colores.length)];
+  };
+
+  let color = obtenerColor();
+  let interval;
+  const timeout = setTimeout(() => {
+    color = obtenerColor();
+    interval = setInterval(() => {
+      let color2 = obtenerColor();
+      while (color2 === color) color2 = obtenerColor();
+
+      color = color2;
+    }, 700 + Math.random() * 100);
+  }, Math.random() * 100);
+
+  onDestroy(() => {
+    clearTimeout(timeout);
+    clearInterval(interval);
+  });
+</script>
+
+<div style={`background-color: #${on ? colorPrincpal : color};`} />
 
 <style>
   div {
     background-color: #49b293;
-  width: 200px;
-  height: 160px;
-  margin: 100px auto 0 auto;
-  transform: rotateX(60deg) rotateY(0deg) rotateZ(-45deg);
+    width: 70px;
+    height: 70px;
+    margin: 1px;
+    border-radius: 5px;
+    /* border-bottom: 1px solid white; */
+    -webkit-transition: background-color 500ms linear;
+    -ms-transition: background-color 500ms linear;
+    transition: background-color 500ms linear;
+    /* transform: rotateX(60deg) rotateY(0deg) rotateZ(-45deg); */
   }
 </style>
