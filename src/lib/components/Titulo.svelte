35aa654830cd3dfb800601460c77a193c52ffe9a
Angellomon <angelmtzdiaz@gmail.com>
2021-08-25 17:09:49 -0500

    feat(fonts): implementación de tipografía oficial

--- src/lib/components/Titulo.svelte
+++ src/lib/components/Titulo.svelte
@@ -1,14 +1,20 @@
 <script>
   import Emphasis from './Emphasis.svelte';
+
+  export let on = false;
 </script>
 
-<h2>scrum <Emphasis>of</Emphasis><br /> scrums</h2>
+<h2 class:on>scrum <Emphasis>of</Emphasis><br /> scrums</h2>
 
 <style>
   h2 {
     color: white;
-    font-size: 5em;
+    font-size: 3em;
     text-align: right;
     line-height: 1em;
+    /* margin-top: -100px; */
+  }
+  .on {
+    color: #3b726c;
   }
 </style>
