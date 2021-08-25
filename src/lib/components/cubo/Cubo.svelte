7c1cee01e1cc373c46efe8b538609319717d0826
Angellomon <angelmtzdiaz@gmail.com>
2021-08-25 17:11:11 -0500

    feat(cubo): implementación de glow y status de on

--- src/lib/components/cubo/Cubo.svelte
+++ src/lib/components/cubo/Cubo.svelte
@@ -1,21 +1,88 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
+  import { fade } from 'svelte/transition';
   import Face from './Face.svelte';
+
+  let glow = false;
+  let on = false;
+
+  const dispatcher = createEventDispatcher();
+
+  const handleHover = () => {
+    glow = true;
+    // console.log('enter');
+  };
+
+  const handleUnhover = () => {
+    glow = false;
+  };
+
+  const handleClick = () => {
+    on = true;
+    dispatcher('click');
+  };
 </script>
 
-<div class="cubo">
+<div class="cubo" on:mouseenter={handleHover} on:mouseleave={handleUnhover} on:click={handleClick}>
+  {#if glow}
+    <span transition:fade class="glow-container" />
+  {/if}
   <span class="top">
-    <Face skewX={30} skewY={0} rotation={-30} scaleY={0.87} />
+    <Face
+      top={50}
+      skewX={30}
+      skewY={0}
+      rotation={-30}
+      scaleY={0.87}
+      {glow}
+      {on}
+      colorPrincpal="3b726c"
+    />
   </span>
   <span class="left">
-    <Face top={30} left={-100} skewX={30} skewY={0} rotation={30} />
+    <Face
+      top={-8.5}
+      left={-30}
+      skewX={30}
+      skewY={0}
+      rotation={30}
+      {glow}
+      {on}
+      colorPrincpal="bfea45"
+    />
   </span>
   <span class="right">
-    <Face top={-273} left={101} skewX={-30} skewY={0} rotation={-30} />
+    <Face
+      top={-66}
+      left={31}
+      skewX={-30}
+      skewY={0}
+      rotation={-30}
+      {glow}
+      {on}
+      colorPrincpal="fdf6b8"
+    />
   </span>
 </div>
 
 <style>
   div.cubo {
     position: relative;
+    /* margin-top: 100px; */
+    align-self: center;
+  }
+
+  div.cubo:hover {
+    cursor: pointer;
+  }
+
+  span.glow-container {
+    margin-left: 60px;
+    margin-top: 120px;
+    z-index: 0;
+    position: absolute;
+
+    -webkit-box-shadow: 0px 0px 39px 50px rgba(191, 234, 69, 0.77);
+    box-shadow: 0px 0px 39px 50px rgba(191, 234, 69, 0.77);
   }
 </style>
