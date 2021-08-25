015e39e0377a75a08ea75a9d80bd3fb8f90c7ea2
Angellomon <angelmtzdiaz@gmail.com>
2021-08-25 17:09:42 -0500

    feat(fonts): implementación de tipografía oficial

--- src/lib/components/Emphasis.svelte
+++ src/lib/components/Emphasis.svelte
@@ -1,11 +1,11 @@
 <span>
-	<slot />
+  <slot />
 </span>
 
 <style>
-	span {
-		font-family: 'Roboto';
-		color: #c3d662;
-		text-shadow: 0 0 1em #c3d662;
-	}
+  span {
+    font-family: 'Invention';
+    color: #c3d662;
+    text-shadow: 0 0 1em #c3d662;
+  }
 </style>
