6ae8c81812c5493e1b0375ac7a78f6e115973204
Angellomon <angelmtzdiaz@gmail.com>
2021-09-02 12:58:14 -0500

    style(titulo): corrección de color

--- src/lib/components/Titulo.svelte
+++ src/lib/components/Titulo.svelte
@@ -4,17 +4,17 @@
   export let on = false;
 </script>
 
-<h2 class:on>scrum <Emphasis>of</Emphasis><br /> scrums</h2>
+<h2 class:on>scrum <br />of<Emphasis>scrum</Emphasis></h2>
 
 <style>
   h2 {
-    color: white;
+    color: #6eceb2;
     font-size: 2.9em;
-    text-align: right;
+    text-align: left;
     line-height: 2.6rem;
     /* margin-top: -100px; */
   }
   .on {
-    color: #3b726c;
+    color: #6eceb2;
   }
 </style>
