0162a4b562b743f035b5e8d5ccf8024319630394
Angellomon <angelmtzdiaz@gmail.com>
2021-09-02 11:36:15 -0500

    style(general): correccińo de diseño

--- src/pages/Principal.svelte
+++ src/pages/Principal.svelte
@@ -29,61 +29,76 @@
   onDestroy(() => clearTimeout(timeout2));
 </script>
 
-<section class:on>
+<section class:on transition:fade>
   {#if !redirect}
-    <div class="container" transition:fade>
+    <span class="titulo">
       <Titulo {on} />
+    </span>
+    <span class="cubo-container">
       <Cubo
         on:click={() => {
           on = true;
         }}
       />
-      {#if on}
-        <div class="mensaje" transition:fade>
-          <p>Powered by Subsidiary</p>
-          <p>of the Future</p>
-        </div>
-      {:else}
-        <span class="helper">dale clic</span>
-      {/if}
-    </div>
+    </span>
+    {#if on}
+      <div class="mensaje">
+        <p transition:fade>Powered by Subsidiary</p>
+        <p transition:fade>of the Future</p>
+      </div>
+    {:else}
+      <span class="helper">dale clic</span>
+    {/if}
   {/if}
 </section>
 
 <style>
+  span.titulo,
+  span.cubo-container,
+  span.helper,
+  div.mensaje {
+    position: absolute;
+  }
+
   p {
     color: #75c7ae;
     text-align: center;
     margin: 0;
     font-weight: bold;
   }
+
+  span.titulo {
+    top: 5%;
+  }
+
+  span.cubo-container {
+    bottom: 50%;
+  }
+
+  span.helper {
+    color: white;
+    text-align: center;
+    bottom: 40%;
+  }
+
   div.mensaje {
     margin-top: 5em;
   }
-  div.container {
-    margin: 5em;
-    padding: 5em;
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-  }
   section {
     display: flex;
-    flex-direction: row;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+
     background-color: #13213d;
     min-width: 100vw;
     min-height: 100vh;
+    justify-content: space-between;
+    position: relative;
   }
   .on {
     background-color: white;
     -webkit-transition: background-color 500ms linear;
     -ms-transition: background-color 500ms linear;
     transition: background-color 500ms linear;
-  }
-  .helper {
-    color: white;
-    text-align: center;
-    margin-top: 15%;
   }
 </style>
